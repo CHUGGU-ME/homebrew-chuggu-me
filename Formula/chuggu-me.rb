a47f0c6ae4feac88bd98602a03c57f9109befd94
itbuddy <skvudrms54@gmail.com>
2023-11-16 01:15:27 +0900

    test brew

--- Formula/chuggu-me.rb
+++ Formula/chuggu-me.rb
@@ -1,14 +1,14 @@
 class ChugguMe < Formula
     desc "epl info for everyone"
     homepage ""
-    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/releases/download/untagged-e1eeb5880af108a0a0e5/CHUGGU-ME-v2-1.0.jar"
+    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/releases/download/test_brew/CHUGGU-ME.jar"
     sha256 "565f87fe2e77d9feba552d087608f51c6511b14b5f0a70a2cc3508479f4c99b1"
     version "1.0"
 
     depends_on "openjdk"
 
     def install
-        jar = "CHUGGU-ME-v2-1.0.jar"
+        jar = "CHUGGU-ME.jar"
         libexec.install "CHUGGU-ME-#{version}.jar" => jar
         (bin/"chuggu-me").write <<~EOS
             #!/bin/bash
