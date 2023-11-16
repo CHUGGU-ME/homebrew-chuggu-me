bd12dfb64f6a3a03429986a44c656cdb5c9b8a23
itbuddy <skvudrms54@gmail.com>
2023-11-17 02:20:22 +0900

    tar.gz로 설치

--- Formula/chuggu-me.rb
+++ Formula/chuggu-me.rb
@@ -1,14 +1,15 @@
 class ChugguMe < Formula
     desc "epl info for everyone"
     homepage ""
-    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/releases/download/test_brew/CHUGGU-ME.jar"
-    sha256 "565f87fe2e77d9feba552d087608f51c6511b14b5f0a70a2cc3508479f4c99b1"
+    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/archive/refs/tags/test_brew_zip.tar.gz"
+    sha256 "77c4b0dcb8e1fb202ebdf76136a8817887257d3303dfda9bca4089dfaecac4af"
     version "1.0"
 
     depends_on "openjdk"
 
     def install
-        libexec.install "CHUGGU-ME.jar"
-        bin.write_jar_script libexec/"CHUGGU-ME.jar", "chuggu-me"
+        system "./gradlew", "jar", "-x", "test"
+        libexec.install "cli/build/libs/CHUGGU-ME-v2.jar"
+        bin.write_jar_script libexec/"CHUGGU-ME-v2.jar", "chuggu-me"
     end
   end
