7bd9b49cf51cd551f1baf4a1820d4f174d7a9d58
itbuddy <skvudrms54@gmail.com>
2023-11-21 22:04:12 +0900

    Update chuggu-me.rb

--- Formula/chuggu-me.rb
+++ Formula/chuggu-me.rb
@@ -1,8 +1,8 @@
 class ChugguMe < Formula
     desc "epl info for everyone"
     homepage ""
-    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/archive/refs/tags/v1.0.18.tar.gz"
-    sha256 "0a157b8766e4ac939ea8e6715b53d6c5fa841d6c7a955f180f9fe4c18e583739"
+    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/archive/refs/tags/v1.1.1.tar.gz"
+    sha256 "b21f54ab959049320908771f0f7a871ada2a2211f91f6bba2d7b9cf76700607a"
     version "1.0"
 
     depends_on "openjdk"
