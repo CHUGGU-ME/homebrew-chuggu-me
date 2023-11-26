68358614ede7ab28c383d05c38ebc92b1689735d
itbuddy <skvudrms54@gmail.com>
2023-11-26 23:24:10 +0900

    1.1.15로 업데이트

--- Formula/chuggume.rb
+++ Formula/chuggume.rb
@@ -1,9 +1,9 @@
 class Chuggume < Formula
     desc "epl info for everyone"
     homepage ""
-    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/archive/refs/tags/v1.1.10.tar.gz"
-    sha256 "9fb7d46d6de379308f3d868f4bea41365903b8241cded6f3df523cf76db29776"
-    version "1.1.10"
+    url "https://github.com/CHUGGU-ME/CHUGGU-ME-v2/archive/refs/tags/v1.1.15.tar.gz"
+    sha256 "bd36bbc782b0363d1a4531a8cd28bac458e20428399ee301e1fef2b8884457d7"
+    version "1.1.15"
 
     depends_on "openjdk"
 
